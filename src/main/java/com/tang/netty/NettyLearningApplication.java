24ff334b1bcee08e99fdf1f3b8171f9c50cb64f7
tangyao <1013654193@qq.com>
2022-04-06 14:35:13 +0800

    改成springboot项目

---
+++ src/main/java/com/tang/netty/NettyLearningApplication.java
@@ -0,0 +1,21 @@
+package com.tang.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Title: com.tang.netty.NettyLearningApplication
+ * @Description:
+ * @author: tangyao
+ * @date: 2022/4/6 10:42
+ * @Version: 1.0
+ */
+
+@SpringBootApplication
+public class NettyLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyLearningApplication.class, args);
+    }
+
+}
