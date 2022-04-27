d43a66bd92b937dee469cd404724639e9ab79144
tangyao <1013654193@qq.com>
2022-04-27 23:09:05 +0800

    p26

--- src/main/java/com/tang/netty/c4/Server3.java
+++ src/main/java/com/tang/netty/c4/Server3.java
@@ -6,12 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
+import java.nio.channels.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import static com.tang.netty.c1.ByteBufferUtil.debugRead;
 
@@ -24,12 +23,18 @@ import static com.tang.netty.c1.ByteBufferUtil.debugRead;
  */
 @Slf4j
 public class Server3 {
+
+
     public static void main(String[] args) throws IOException {
+
+        // accept -会在有链接请求时触发
+        // connect -是客户端，连接建立后触发
+        // read -可读事件
+        // write -可写事件
 
         //1、创建selector , 管理多个channel
         Selector selector = Selector.open();
 
-        ByteBuffer buffer = ByteBuffer.allocate(16);
         ServerSocketChannel ssc = ServerSocketChannel.open();
         ssc.configureBlocking(false);
 
@@ -38,10 +43,24 @@ public class Server3 {
         // SelectionKey 就是将来事件发生后，通过它可以知道事件和哪个channel的事件
         SelectionKey sscKey = ssc.register(selector, 0, null);
         sscKey.interestOps(SelectionKey.OP_ACCEPT);
-
+        log.debug("register key:{}", sscKey);
         ssc.bind(new InetSocketAddress(8080));
         while (true) {
-            // 3、select 方法
+            // 3、select 方法,没有事件发生，线程阻塞，有事件。线程才会回复运行
+            selector.select();
+
+            //4、处理事件,selectedKeys 内部包含了所有发生的事件
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                log.debug("key:{}", key);
+                ServerSocketChannel channel = (ServerSocketChannel) key.channel();
+                SocketChannel sc = channel.accept();
+                log.debug("{}", sc);
+
+            }
+
+
         }
 
 
